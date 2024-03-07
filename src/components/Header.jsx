01c00bc53ad1f477a5b5a3a3ce2cf4526396b718
unknown <merajurrahmanniber@gmail.com>
2024-03-07 17:36:05 +0600

    header.jsx & 21:00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import styled from 'styled-components';
+import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <div>Header</div>
   )
 }
+
+const Container = styled.div``;
 
 export default Header
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,25 +6,41 @@ const Signup = () => {
   return (
     <Container>
         <BackgroundImage />
-        <Header />
-        <div className="body flex column a-center j-center">
-            <div className="text flex column">
-                <h1>Unlimited Movies Tv shows and more.</h1>
-                <h4>Wtach anywhere, Cancel anytime.</h4>
-                <h6>
-                    Ready to watch? Enter your email to create or reset membership
-                </h6>
+        <div className="content">
+
+        
+            <Header />
+            <div className="body flex column a-center j-center">
+                <div className="text flex column">
+                    <h1>Unlimited Movies Tv shows and more.</h1>
+                    <h4>Wtach anywhere, Cancel anytime.</h4>
+                    <h6>
+                        Ready to watch? Enter your email to create or reset membership
+                    </h6>
+                </div>
+                <div className="form">
+                    <input type="email" placeholder='Enter email address'  name='email'/>
+                    <input type="password" placeholder='Enter password' name='password' />
+                    <button>Get Started</button>
+                </div>
+                <button>Log In</button>
             </div>
-            <div className="form">
-                <input type="email" placeholder='Enter email address'  name='email'/>
-                <input type="password" placeholder='Enter password' name='password' />
-                <button>Get Started</button>
-            </div>
-            <button>Log In</button>
         </div>
     </Container>
   )
 }
-const Container = styled.div``;
+const Container = styled.div`
+    position: relative;
+    .content{
+        position: absolute;
+        top:0;
+        left: 0
+        background-color: rgba(0,0,0,0.5);
+        height: 100vh;
+        width:100vw;
+        display: grid;
+        grit-template-rows:15vh 85vh; 
+    }
+`;
 
 export default Signup
