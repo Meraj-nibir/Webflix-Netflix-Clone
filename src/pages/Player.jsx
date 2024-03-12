a1b5511428abd737c0a1b40ca5d3c4e49546bf68
unknown <merajurrahmanniber@gmail.com>
2024-03-12 15:47:39 +0600

    index.jsx & 1:33:43

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
-import {BsArrowLeft} from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom'
-import trailer from '../assets/trailer.mp4'
+import React from 'react';
+import styled from 'styled-components';
+import {BsArrowLeft} from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+import trailer from '../assets/trailer.mp4';
+
 export default function Player() {
     const navigate = useNavigate();
   return (
     <Container>
         <div className="player">
             <div className="back">
-                <BsArrowLeft onClick={()=> navigate(-1)}/>
+                <BsArrowLeft onClick={() => navigate(-1)} />
             </div>
-            <video src={trailer} autoplay loop controls muted></video>
+            <video src={trailer}  autoPlay loop controls muted  />
         </div>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
-
+  .player {
+    width: 100vw;
+    height: 100vh;
+    .back {
+      position: absolute;
+      padding: 2rem;
+      z-index: 1;
+      svg {
+        font-size: 3rem;
+        cursor: pointer;
+      }
+    }
+    video {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
 `;
