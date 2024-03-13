12111523998fa1c3850b23ea177658f4aec5689d
unknown <merajurrahmanniber@gmail.com>
2024-03-13 15:52:16 +0600

    Card.jsx & 2:06:36

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Card(movieData, isLiked = false ) {
+  return <Container>
+    <img src={`https://image.tmdb.org/t/p/w500${movieData.image}`} alt="movie" />
+  </Container>
+}
+const Container = styled.div``;
