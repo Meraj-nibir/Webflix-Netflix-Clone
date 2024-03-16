d81cc3f9c13cdca90f25d91003ed81c3b5f02a47
unknown <merajurrahmanniber@gmail.com>
2024-03-16 15:27:25 +0600

    CardSlider.jsx & 2:35:42

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import CardSlider from "./CardSlider";
+
+
 export default function Slider({ movies }) {
   const getMoviesFromRange = (from, to) => {
     return movies.slice(from, to);
